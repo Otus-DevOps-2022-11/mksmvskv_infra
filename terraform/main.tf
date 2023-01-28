b6321c0d78f125f5afcc7e18693e469f4f992165
mksmvskv <latetoolate@gmail.com>
2023-01-28 03:06:24 +0300

    fix provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,18 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-  required_version = ">= 0.13"
-}
-
-# provider "yandex" {
-#   #service_account_key_file = "key2.json"
-#   token  = var.token
-#   cloud_id  = var.cloud_id
-#   folder_id = var.folder_id
-#   zone      = var.zone
+# terraform {
+#   required_providers {
+#     yandex = {
+#       source = "yandex-cloud/yandex"
+#     }
+#   }
+#   required_version = ">= 0.13"
 # }
+
+provider "yandex" {
+  token  = var.token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
+}
 
 resource "yandex_compute_instance" "app" {
   name = "reddit-app"
