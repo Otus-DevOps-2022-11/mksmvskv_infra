0a862cfd3de6b188567aa07124bda992368f798c
mksmvskv <latetoolate@gmail.com>
2023-02-08 15:51:48 +0300

    fix main.tf

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -6,18 +6,18 @@
 #   }
 #   required_version = ">= 0.13"
 
-backend "s3" {
-    endpoint   = "storage.yandexcloud.net"
-    bucket     = "mksm-bucket-home-test"
-    region     = "ru-central1"
-    key        = "test/terraform/prod/prod.tfstate"
-    access_key = var.access_key
-    secret_key = var.secret_key
-    skip_region_validation      = true
-    skip_credentials_validation = true
-  }
+# backend "s3" {
+#     endpoint   = "storage.yandexcloud.net"
+#     bucket     = "mksm-bucket-home-test"
+#     region     = "ru-central1"
+#     key        = "test/terraform/prod/prod.tfstate"
+#     access_key = var.access_key
+#     secret_key = var.secret_key
+#     skip_region_validation      = true
+#     skip_credentials_validation = true
+#   }
 
-}
+# }
 
 provider "yandex" {
   token     = var.token
