35e9438994dee3a0e0e671084df15a84e4fa894e
mksmvskv <latetoolate@gmail.com>
2023-03-12 18:45:03 +0300

    Ansible-2 Homework

--- ansible/di.sh
+++ ansible/di.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-yc compute instances list | tail -n +4 | head -n -2 | awk -F '|' '{print "["$3"]\n" "ansible_host="$6}'| tr -d '[:blank:]'
-python di_python.py
+yc compute instances list | tail -n +4 | head -n -2 | grep reddit | awk -F '|' '{print "["$3"]\n" "ansible_host="$6}'| tr -d '[:blank:]' > inventory
+python3 di_python.py
